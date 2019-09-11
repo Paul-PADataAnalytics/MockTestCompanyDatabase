590ba62321c5ae9561361db8f29ec714d62afc87
Paul - PA Data Analytics <53405381+Paul-PADataAnalytics@users.noreply.github.com>
2019-09-11 15:49:52 +0100

    built PowerBI file and data dump

--- SimulateCustomer.py
+++ SimulateCustomer.py
@@ -12,8 +12,8 @@ import world as world
 import database as DB
 import sys
 
-#cityname = sys.argv[1]
-cityname = 'Henna'
+#cityname = 'Henna'
+cityname = sys.argv[1]
 localdebug = False
 
 def simulateCity(cityname):
