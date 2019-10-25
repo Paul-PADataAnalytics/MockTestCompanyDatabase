d663d5a3a3f0766ce556dc6104311d270d216fa4
Paul Allen <paul@padanalytics.co.uk>
2019-10-25 18:14:56 +0100

    Removed Duplicate files now that we're organised

--- resetdatabase.bat
+++
@@ -1,4 +0,0 @@
-"c:\Program Files\MariaDB 10.4\bin\mysql.exe" -u root -p  < forwardengineeredmodel.sql
-"c:\Program Files\MariaDB 10.4\bin\mysql.exe" -u root -p  < mandatesandcertifications.sql
-"c:\Program Files\MariaDB 10.4\bin\mysql.exe" -u root -p < partcertifications.sql
-"c:\Program Files\MariaDB 10.4\bin\mysql.exe" -u root -p < testcentre.sql
